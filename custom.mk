010e91d720251902cf5f774d4c6b2383753f8511
John Mulligan <jmulligan@redhat.com>
2023-02-10 10:29:58 -0500

    xxx: more hacks
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- custom.mk
+++ custom.mk
@@ -1,10 +1,24 @@
 
 include Makefile
 
+H=a34c560605b
+
 CUSTOM_SERVER_BUILD_ARGS = \
 	--build-arg=INSTALL_PACKAGES_FROM=custom-repo \
-	--build-arg=INSTALL_CUSTOM_REPO=http://localhost:8000/jjm-hack1.repo \
+	--build-arg=INSTALL_CUSTOM_REPO=http://localhost:8000/jjm-hack-$H.repo \
 	--net=host
+
+.PHONY: a
+a:
+	$(MAKE) -f custom.mk b
+	podman push quay.io/samba.org/samba-server:dev  docker://localhost:5555/samba.org/samba-server:vfs
+
+.PHONY: b
+b: repo-file build-custom-server
+
+.PHONY: repo-file
+repo-file:
+	(cd ~/tmp/samba.out.d/jjm-hack1/RPMS/ && createrepo . && cp jjm-hack1.repo jjm-hack-$H.repo)
 
 .PHONY: build-custom-server
 build-custom-server:
@@ -17,3 +31,6 @@ build-custom-server:
 		DIR=$(SERVER_DIR) \
 		BUILDFILE=.custom.build
 
+.PHONY: rpms
+rpms:
+	python ./images/fromsource/build.py -c a.yaml
