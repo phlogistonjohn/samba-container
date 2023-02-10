41057af7d8468b88815111d32e62e345775bbe33
John Mulligan <jmulligan@redhat.com>
2023-02-10 12:36:02 -0500

    xxx: images/fromsource/build.py
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- images/fromsource/build.py
+++ images/fromsource/build.py
@@ -4,15 +4,17 @@ import argparse
 import importlib.machinery
 import logging
 import logging
+import os
 import pathlib
 import sys
 
 import yaml
 
 log = logging.getLogger()
+_mydir = pathlib.Path(__file__).parent
 
 try:
-    sibling = pathlib.Path(__file__).parent / "samba-from-source.py"
+    sibling = _mydir / "samba-from-source.py"
     _sfs = importlib.machinery.SourceFileLoader(
         "sfs", str(sibling)
     ).load_module()
@@ -23,117 +25,9 @@ except ImportError:
 run = _sfs.run
 
 
-def host_build_image(cli, engine):
-    cmd = [engine, "build"]
-    dnf_args = dnf_cache(cli)
-    if cli.base_image:
-        cmd.append(f"--build-arg=BASE_IMAGE={cli.base_image}")
-    if dnf_args:
-        cmd.append(f"--build-arg=EXTRA_OPTS=--keep-dnf")
-    tag = "dev"
-    if cli.job:
-        tag = f"dev-{cli.job}"
-    cmd.append("-t")
-    cmd.append(f"samba-from-source:{tag}")
-    cmd.extend(dnf_args)
-    cmd.append("-f")
-    # TODO: auto-fix path
-    cmd.append("images/fromsource/Containerfile")
-    run(cmd)
-
-
-def host_build_samba(cli, engine):
-    tag = "dev"
-    if cli.job:
-        tag = f"dev-{cli.job}"
-    img = f"samba-from-source:{tag}"
-    cmd = [engine, "run", "--rm", "-it"]
-    cmd.extend(dnf_cache(cli))
-    if cli.source_dir:
-        cmd.append("--volume")
-        cmd.append(f"{cli.source_dir}:{cli.samba_source}")
-    if cli.artifacts_dir:
-        cmd.append("--volume")
-        cmd.append(f"{cli.artifacts_dir}:{cli.workdir}")
-    cmd.append("--userns=keep-id")
-    if cli.shell:
-        cmd.extend(["--entrypoint", "bash"])
-    cmd.append(img)
-    if not cli.shell:
-        cmd.extend(Wrapper(cli).to_args())
-    run(cmd)
-
-
-def get_engine(cli):
-    if cli.container_engine:
-        return cli.container_engine
-    # TODO: auto detect podman or docker
-    return "podman"
-
-
-def dnf_cache(cli):
-    if not cli.dnf_cache:
-        return []
-    libdir = pathlib.Path(cli.dnf_cache) / "lib"
-    cdir = pathlib.Path(cli.dnf_cache) / "cache"
-    libdir.mkdir(parents=True, exist_ok=True)
-    cdir.mkdir(parents=True, exist_ok=True)
-    return [
-        "--volume",
-        f"{libdir}:/var/lib/dnf",
-        "--volume",
-        f"{cdir}:/var/cache/dnf",
-    ]
-
-
-def parse_cli():
-    parser = argparse.ArgumentParser()
-    _sfs.set_arguments(parser)
-    parser.add_argument(
-        "--container-engine",
-        help="Specify container engine to use (typically docker, or podman)",
-    )
-    parser.add_argument(
-        "--source-dir", "-s", help="Path to samba git checkout"
-    )
-    parser.add_argument(
-        "--artifacts-dir",
-        "-a",
-        help="Where stuff gets written",
-    )
-    parser.add_argument(
-        "--base-image",
-        help="Base image (example: quay.io/centos/centos:stream9)",
-    )
-    parser.add_argument(
-        "--task",
-        "-t",
-        action="append",
-        choices=("image", "packages", "configure", "make"),
-        help="What to build",
-    )
-    parser.add_argument(
-        "--dnf-cache",
-        help="Path to a directory for caching dnf state",
-    )
-    parser.add_argument(
-        "--shell", action="store_true", help="Get a shell in the container"
-    )
-    parser.add_argument("--config", "-c", help="Provide a configuration file")
-    cli = parser.parse_args()
-    return cli
-
-
-def _do_task(cli, task_name, is_default=True):
-    if not cli.task and is_default:
-        log.debug("default tasks enabled; running task %s", task_name)
-        return True
-    ok = task_name in list(cli.task)
-    log.debug("%srunning task %s", "" if ok else "not ", task_name)
-    return ok
-
-
 class Configurator:
+    """Merges CLI configuration and YAML file configuration."""
+
     def __init__(self, file=None, cli=None):
         self._cli = cli
         self._conf = None
@@ -199,11 +93,20 @@ class Configurator:
     def shell(self):
         return self._cli and self._cli.shell
 
+    @property
+    def example_yaml(self):
+        return self._cli and self._cli.example_yaml
+
     def real_cli(self):
         return self._cli
 
 
 class Wrapper:
+    """Combines the cli/config from build.py with the CLI arguments from
+    samba-from-source.py."""
+
+    _unset = object()
+
     def __init__(self, cli):
         self._cli = cli
         self._rcli = None
@@ -213,30 +116,191 @@ class Wrapper:
         self._out = []
 
     def fetch(self, name):
-        val = getattr(self._cli, name, Wrapper)
-        if val is not Wrapper:
+        """Return a config value from either the combined setting or
+        from the actual CLI.
+        """
+        val = getattr(self._cli, name, _unset)
+        if val is not _unset:
             return val
         if self._rcli:
-            val = getattr(self._rcli, name, Wrapper)
-            if val is not Wrapper:
+            val = getattr(self._rcli, name, _unset)
+            if val is not _unset:
                 return val
         return None
 
-    def add_argument(self, opt, *args, action=None, **kwargs):
-        name = opt[2:].replace("-", "_")
-        if action == "store_true":
-            if self.fetch(name):
-                self._out.append(opt)
-            return
-        val = self.fetch(name)
-        if val:
-            self._out.append(opt)
-            self._out.append(val)
-
     def to_args(self):
+        """Return a list of arguments to pass to samba-from-source.py."""
+
+        class _MergeArgs:
+            def __init__(self, wrapper):
+                self.out = []
+                self.wrapper = wrapper
+
+            def add_argument(self, opt, *args, action=None, **kwargs):
+                """Callback function that will be used by set_arguments."""
+                name = opt[2:].replace("-", "_")
+                if action == "store_true":
+                    if self.wrapper.fetch(name):
+                        self.out.append(opt)
+                    return
+                val = self.fetch(name)
+                if val:
+                    self.out.append(opt)
+                    self.out.append(val)
+
+        m = _MergeArgs(self)
         if not self._out:
-            _sfs.set_arguments(self)
-        return self._out
+            _sfs.set_arguments(m)
+        return m.out
+
+
+def build_image(cli, engine):
+    """Build the container image that will later be used to build samba packages."""
+    cmd = [engine, "build"]
+    dnf_args = dnf_cache(cli)
+    if cli.base_image:
+        cmd.append(f"--build-arg=BASE_IMAGE={cli.base_image}")
+    if dnf_args:
+        cmd.append(f"--build-arg=EXTRA_OPTS=--keep-dnf")
+    tag = "dev"
+    if cli.job:
+        tag = f"dev-{cli.job}"
+    cmd.append("-t")
+    cmd.append(f"samba-from-source:{tag}")
+    cmd.extend(dnf_args)
+    cmd.append("-f")
+    cmd.append(str(_mydir / "Containerfile"))
+    run(cmd)
+
+
+def build_samba(cli, engine):
+    """Build the samba packages using a container image."""
+    tag = "dev"
+    if cli.job:
+        tag = f"dev-{cli.job}"
+    img = f"samba-from-source:{tag}"
+    cmd = [engine, "run", "--rm", "-it"]
+    cmd.extend(dnf_cache(cli))
+    if cli.source_dir:
+        cmd.append("--volume")
+        cmd.append(f"{cli.source_dir}:{cli.samba_source}")
+    if cli.artifacts_dir:
+        cmd.append("--volume")
+        cmd.append(f"{cli.artifacts_dir}:{cli.workdir}")
+    cmd.append("--userns=keep-id")
+    if cli.shell:
+        cmd.extend(["--entrypoint", "bash"])
+    cmd.append(img)
+    if not cli.shell:
+        cmd.extend(Wrapper(cli).to_args())
+    run(cmd)
+
+
+def get_engine(cli):
+    """Return container engine to use."""
+    if cli.container_engine:
+        return cli.container_engine
+    for eng in ["podman", "docker"]:
+        try:
+            with open(os.devnull, "w") as nullfh:
+                run([eng, "--help"], check=True, stdout=nullfh, stderr=nullfh)
+            return eng
+        except FileNotFoundError:
+            continue
+    raise ValueError("unable to detect a container engine")
+
+
+def dnf_cache(cli):
+    if not cli.dnf_cache:
+        return []
+    libdir = pathlib.Path(cli.dnf_cache) / "lib"
+    cdir = pathlib.Path(cli.dnf_cache) / "cache"
+    libdir.mkdir(parents=True, exist_ok=True)
+    cdir.mkdir(parents=True, exist_ok=True)
+    return [
+        "--volume",
+        f"{libdir}:/var/lib/dnf",
+        "--volume",
+        f"{cdir}:/var/cache/dnf",
+    ]
+
+
+def show_yaml(cli):
+    print(
+        """---
+container_engine: podman
+job: centos9
+base_image: quay.io/centos/centos:stream9
+dnf_cache: /home/example/tmp/dnf/centos9
+source_dir: /home/example/devel/samba
+artifacts_dir: /home/example/tmp/samba.out.d
+tasks:
+  - image
+  - packages
+"""
+    )
+
+
+def parse_cli():
+    parser = argparse.ArgumentParser(
+        description="""
+Automate building samba packages using a container. This script makes
+use of samba-from-source.py in the same directory.
+
+It can be controlled on the CLI or aided by a YAML configuaration file.
+Use --help-yaml to see an example.
+"""
+    )
+    _sfs.set_arguments(parser)
+    parser.add_argument(
+        "--container-engine",
+        help="Specify container engine to use (typically docker, or podman)",
+    )
+    parser.add_argument(
+        "--source-dir", "-s", help="Path to samba git checkout"
+    )
+    parser.add_argument(
+        "--artifacts-dir",
+        "-a",
+        help="Path to a local directory where output will be saved",
+    )
+    parser.add_argument(
+        "--base-image",
+        help="Base image (example: quay.io/centos/centos:stream9)",
+    )
+    parser.add_argument(
+        "--task",
+        "-t",
+        action="append",
+        choices=("image", "packages"),  # TODO: "configure", "make"
+        help="What to build",
+    )
+    parser.add_argument(
+        "--dnf-cache",
+        help="Path to a directory for caching dnf state",
+    )
+    parser.add_argument(
+        "--shell",
+        action="store_true",
+        help="Interrupt package build and get a shell within the container instead.",
+    )
+    parser.add_argument(
+        "--example-yaml",
+        action="store_true",
+        help="Display example configuration yaml",
+    )
+    parser.add_argument("--config", "-c", help="Provide a configuration file")
+    cli = parser.parse_args()
+    return cli
+
+
+def _do_task(cli, task_name, is_default=True):
+    if not cli.task and is_default:
+        log.debug("default tasks enabled; running task %s", task_name)
+        return True
+    ok = task_name in list(cli.task)
+    log.debug("%srunning task %s", "" if ok else "not ", task_name)
+    return ok
 
 
 def main():
@@ -247,11 +311,15 @@ def main():
     )
     cli = Configurator(cli.config, cli=cli)
 
+    if cli.example_yaml:
+        show_yaml(cli)
+        sys.exit(0)
+
     engine = get_engine(cli)
     if _do_task(cli, "image"):
-        host_build_image(cli, engine)
+        build_image(cli, engine)
     if _do_task(cli, "packages"):
-        host_build_samba(cli, engine)
+        build_samba(cli, engine)
     sys.exit(0)
 
 
